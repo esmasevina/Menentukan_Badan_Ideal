dc53476e6d5b644ee2d46fdc635cd10b008c3750
esmasevina <esmanursevina78@gmail.com>
2024-10-16 16:49:53 +0700

    Program sederhana belajar java dengan program penentuan badan ideal

---
+++ src/programmenentukanbadanideal/ProgramHitungBeratBadan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmenentukanbadanideal;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class ProgramHitungBeratBadan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Frame_Badan_Ideal Laund = new Frame_Badan_Ideal();
+        Laund.setVisible(true);
+    }
+}
